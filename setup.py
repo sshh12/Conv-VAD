8bd24d41171bf6b008407a85d355ac6eb28708a0
Shrivu Shankar <shrivu1122@gmail.com>
2019-05-24 17:09:28 -0500

    Oops

--- setup.py
+++ setup.py
@@ -9,12 +9,6 @@ except:
 
 
 def join(*paths):
-    """Join and normalize several paths.
-    Args:
-        *paths (List[str]): The paths to join and normalize.
-    Returns:
-        str: The normalized path.
-    """
     return os.path.normpath(os.path.join(*paths))
 
 
@@ -22,16 +16,10 @@ VERSION_PATH = join(__file__, '..', 'conv_vad', 'version.py')
 
 
 def get_version():
-    """Get the version number without running version.py.
-    Returns:
-        str: The current uavaustin-target-finder version.
-    """
 
     with open(VERSION_PATH, 'r') as version:
         out = {}
-
         exec(version.read(), out)
-
         return out['__version__']
 
 
